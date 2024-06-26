f090ad1aa220b193442b634014522a6542275076
Luis Illanes <illanesluis18@gmail.com>
2024-06-26 10:06:08 -0400

    correccion precio

--- patitasYCompania/models.py
+++ patitasYCompania/models.py
@@ -7,7 +7,7 @@ from django.contrib.contenttypes.models import ContentType
 class Accesorio(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    precio = models.DecimalField(max_digits=10, decimal_places=0)
     disponible = models.BooleanField(default=True)
     creado_en = models.DateTimeField(auto_now_add=True)
     actualizado_en = models.DateTimeField(auto_now=True)
